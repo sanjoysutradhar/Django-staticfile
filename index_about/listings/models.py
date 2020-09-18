263c0a39812363b9513d7c16c490095d11f3c80f
shanjoy sutradhar <shanjoy35-2025@diu.edu.bd>
2020-09-18 20:42:16 +0600

    Listings show in project

--- index_about/listings/models.py
+++ index_about/listings/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 from realtors.models import Realtor
 
@@ -21,13 +20,16 @@ class listings(models.Model):
     is_published = models.BooleanField()
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d')
     photo1 = models.ImageField(upload_to='photos/%Y/%m/%d')
-    photo2 = models.ImageField(upload_to='photos/%Y/%m/%d')
-    photo3 = models.ImageField(upload_to='photos/%Y/%m/%d')
-    photo4 = models.ImageField(upload_to='photos/%Y/%m/%d')
-    photo5 = models.ImageField(upload_to='photos/%Y/%m/%d')
-    photo6 = models.ImageField(upload_to='photos/%Y/%m/%d')
-    list_date = models.DateTimeField(auto_now_add=True)
+    photo2 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo3 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo4 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo5 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo6 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    list_date = models.DateTimeField(auto_now_add=False)
     update = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ("-list_date",)
--- index_about/listings/views.py
+++ index_about/listings/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import listings
 
 
 # Create your views here.
 
 def listings_index(request):
-    return render(request, 'listings/listings.html')
+    listings_list = listings.objects.all()
+    context = {
+        'listings_list': listings_list,
+    }
+    return render(request, 'listings/listings.html',context)
+    #return render(request, 'listings/listings.html',{'listings_list': listings_list,})
 
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request,listing_id):
+    listing=listings.objects.get(id=listing_id)
+    context = {
+        'listing':listing,
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
